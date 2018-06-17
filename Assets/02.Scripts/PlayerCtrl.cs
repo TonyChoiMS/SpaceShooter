308a7613770b1b0294bbcf58918cec9e97af3793
TonyChoiMS <cms9286@gmail.com>
2018-06-17 20:15:30 +0900

    first commit
    create map
    create player

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+    float h = 0.0f;
+    float v = 0.0f;
+
+    // Component must be used to assignment variable
+    [SerializeField] Transform tr;
+    // move speed variable (exposed to Inspector assign public)
+    public float moveSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        // Initialize Component
+        tr = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("h=" + h.ToString());
+        Debug.Log("v=" + v.ToString());
+
+        // Translate(MoveDirection * speed * displacement * Time.deltaTime, standard location)
+        tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
+	}
+}
