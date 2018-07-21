b7087d3083213287b4d7ed6ec61d311fefffae3b
TonyChoiMS <cms9286@gmail.com>
2018-07-21 18:16:36 +0900

    commit enemy character

---
+++ Assets/02.Scripts/Enemy/MoveAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAgent : MonoBehaviour {
+
+    // patrol way point list
+    public List<Transform> wayPoints;
+    // next way point index
+    public int nextIdx;
+
+	// Use this for initialization
+	void Start () {
+        var group = GameObject.Find("WaypointGroup");
+        if (group != null)
+        {
+            // export all transform component in waypointgroup 
+            // add list
+            group.GetComponentsInChildren<Transform>(wayPoints);
+            // remove first child
+            wayPoints.RemoveAt(0);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
