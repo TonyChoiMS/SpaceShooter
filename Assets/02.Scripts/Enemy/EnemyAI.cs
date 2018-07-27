d709dc7eb6a87c355cafcab2e53f2b3163f3349e
TonyChoi <cms9286@gmail.com>
2018-07-27 10:56:04 +0900

    commit enemy AI

---
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    // Enemy State enum
+    public enum State
+    {
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    // save state variable
+    public State state = State.PATROL;
+
+
+    Transform m_trPlayer;       // player transfrom
+    Transform m_trEnemy;        // Enemy transform
+
+    public float m_flAttackDist = 5.0f;
+    public float m_flTraceDist = 10.0f;
+
+    public bool m_bIsDie = false;
+
+    WaitForSeconds ws;
+
+    MoveAgent m_moveAgent;
+
+    void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag("PLAYER");
+        if (player != null)
+        {
+            m_trPlayer = player.GetComponent<Transform>();
+        }
+
+        m_trEnemy = GetComponent<Transform>();
+        m_moveAgent = GetComponent<MoveAgent>();
+
+        ws = new WaitForSeconds(0.3f);
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+    }
+
+    IEnumerator CheckState()
+    {
+        while (!m_bIsDie)
+        {
+            if (state == State.DIE) yield break;
+
+            float dist = Vector3.Distance(m_trPlayer.position, m_trEnemy.position);
+
+            if (dist <= m_flAttackDist)
+            {
+                state = State.ATTACK;
+            }
+            else if (dist <= m_flTraceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.PATROL;
+            }
+
+            yield return ws;
+        }
+    }
+
+    IEnumerator Action()
+    {
+        while (!m_bIsDie)
+        {
+            yield return ws;
+
+            switch (state)
+            {
+                case State.PATROL:
+                    break;
+                case State.TRACE:
+                    break;
+                case State.ATTACK:
+                    break;
+                case State.DIE:
+                    break;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/02.Scripts/Enemy/MoveAgent.cs
+++ Assets/02.Scripts/Enemy/MoveAgent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MoveAgent : MonoBehaviour {
 
@@ -9,8 +10,24 @@ public class MoveAgent : MonoBehaviour {
     // next way point index
     public int nextIdx;
 
+    readonly float patrolSpeed = 1.5f;
+    readonly float traceSpeed = 4.0f;
+
+    private NavMeshAgent agent;
+
+    bool m_bPatrolling;
+    bool Patrolling { get { return m_bPatrolling; } set { m_bPatrolling = value; if (m_bPatrolling) { agent.speed = patrolSpeed; MoveWayPoint(); } } }
+
+    Vector3 m_vtTraceTarget;
+
+
 	// Use this for initialization
 	void Start () {
+        // NavMeshAgent Initialize
+        agent = GetComponent<NavMeshAgent>();
+        // autoBraking option off
+        agent.autoBraking = false;
+
         var group = GameObject.Find("WaypointGroup");
         if (group != null)
         {
@@ -20,10 +37,29 @@ public class MoveAgent : MonoBehaviour {
             // remove first child
             wayPoints.RemoveAt(0);
         }
+
+        MoveWayPoint();
 	}
+
+    void MoveWayPoint()
+    {
+        // wait calculate path
+        if (agent.isPathStale) return;
+
+        agent.destination = wayPoints[nextIdx].position;
+        // turn on navigation
+        agent.isStopped = false;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+    {
+        if (agent.velocity.sqrMagnitude >= 0.2f * 0.2f
+            && agent.remainingDistance <= 0.5f)
+        {
+            nextIdx = ++nextIdx & wayPoints.Count;
+            // move next point
+            MoveWayPoint();
+        }
 	}
 }
