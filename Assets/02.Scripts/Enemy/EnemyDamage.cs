18669372d50129647ff49673cb5fdd1c7f2be3e9
TonyChoi <cms9286@gmail.com>
2018-08-17 18:07:19 +0900

    add enemy hp bar

--- Assets/02.Scripts/Enemy/EnemyDamage.cs
+++ Assets/02.Scripts/Enemy/EnemyDamage.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class EnemyDamage : MonoBehaviour
 {
     private const string bulletTag = "BULLET";
     //생명 게이지
     private float hp = 100.0f;
+
+    private float initHp = 100.0f;
     //피격 시 사용할 혈흔 효과
     private GameObject bloodEffect;
+
+    // 생명 게이지 프리팹을 저장할 변수
+    public GameObject hpBarPrefab;
+    // 생명 게이지의 위치를 보정할 오프셋
+    public Vector3 hpBarOffset = new Vector3(0, 2.2f, 0);
+    // 부모가 될 Canvas 객체
+    private Canvas uiCanvas;
+    // 생명 수치에 따라 fillAmount 속성을 변경할 Image
+    private Image hpBarImage;
 
     void Start()
     {
         //혈흔 효과 프리팹을 로드
         bloodEffect = Resources.Load<GameObject>("BulletImpactFleshBigEffect");
+
+        SetHpBar();
+    }
+
+    void SetHpBar()
+    {
+        uiCanvas = GameObject.Find("UI Canvas").GetComponent<Canvas>();
+        // UI Canvas 하위로 생명 게이지 생성
+        GameObject hpBar = Instantiate<GameObject>(hpBarPrefab, uiCanvas.transform);
+        // fillAmount 속성을 변경할 Image 추출
+        hpBarImage = hpBar.GetComponentsInChildren<Image>()[1];
+
+        // 생명 게이지가 따라가야 할 대상과 오프셋 값 설정
+        var _hpBar = hpBar.GetComponent<EnemyHpBar>();
+        _hpBar.targetTr = this.gameObject.transform;
+        _hpBar.offset = hpBarOffset;
     }
 
     void OnCollisionEnter(Collision coll)
@@ -26,11 +55,14 @@ public class EnemyDamage : MonoBehaviour
             coll.gameObject.SetActive(false);
             //생명 게이지 차감
             hp -= coll.gameObject.GetComponent<BulletCtrl>().damage;
+            // 생명 게이지의 fillAmount 속성을 변경
+            hpBarImage.fillAmount = hp / initHp;
 
             if (hp <= 0.0f)
             {
                 //적 캐릭터의 상태를 DIE로 변경
                 GetComponent<EnemyAI>().state = EnemyAI.State.DIE;
+                hpBarImage.GetComponentsInParent<Image>()[1].color = Color.clear;
             }
         }
     }
